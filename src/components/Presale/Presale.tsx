30b6d00b018236506fb0e330629db635c6fbde02
themms <maximusnewera@mail.ru>
2024-04-30 23:46:36 +0600

    Пофиксил получение id

--- src/components/Presale/Presale.tsx
+++ src/components/Presale/Presale.tsx
@@ -21,10 +21,6 @@ export const Presale = () => {
   const handleStateBuyMint = () => {
     setIsMintState(!isMintState);
   };
-  //@ts-ignore
-  const tg = window.Telegram.WebApp;
-  console.log(tg);
-  const tgId = tg?.initDataUnsafe?.user?.id;
 
   //функция открытия и закрытия попапа info
   const handleStateInfo = () => {
@@ -38,7 +34,7 @@ export const Presale = () => {
     <section className="presale">
       <div className="presale__container">
         <h1 className="presale__title">
-          <span className="presale__title-span">Limited {tgId ? tgId : "Нету"}</span>
+          <span className="presale__title-span">Limited</span>
           <br />
           <span className="presale__title-span">Minting</span>
         </h1>

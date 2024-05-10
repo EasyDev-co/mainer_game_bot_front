c59c817e53bb72fad1bcbe551de51414332fad8d
themms <maximusnewera@mail.ru>
2024-05-10 22:20:26 +0600

    test

--- src/components/TestAdress.tsx
+++ src/components/TestAdress.tsx
@@ -1,14 +1,16 @@
-import { useTonWallet } from '@tonconnect/ui-react';
+import { useTonAddress, useTonWallet } from '@tonconnect/ui-react';
 
 export const Address = () => {
   const wallet = useTonWallet();
+  const userFriendlyAddress = useTonAddress();
+  const rawAddress = useTonAddress(false);
 
   console.log(wallet);
 
-  return wallet && (
+  return (
     <div>
-      <span>Connected wallet: {wallet?.device.platform}</span>
-      <span>Device: {wallet.device.appName}</span>
+      <span>User-friendly address: {userFriendlyAddress}</span>
+      <span>Raw address: {rawAddress}</span>
     </div>
   );
 };
